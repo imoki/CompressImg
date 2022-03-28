ee4725ca0cc94be923498ccdbac79baca9743ea4
imoki <207586414@qq.com>
2022-03-29 00:00:53 +0800

    v1.1.0

--- compressimg.py
+++ compressimg.py
@@ -14,7 +14,19 @@ def imagesize(file):	# 计算图片占用空间
 	size = os.path.getsize(file)/1024 # kb -> b
 	return size
 	
+def createdir(outfile):	# 传入文件路径，递归创建文件夹
+	outpath = outfile.split("\\")
+	outpath.pop(-1)
+	outpath = "\\".join(outpath)
+	if not os.path.exists(outfile):
+		try:
+			os.makedirs(outpath)
+		except:
+			#print("目录已存在")
+			pass
+
 def compress(infile, outfile, sizeKB, xout, step=10, quality=80):	# 图片压缩
+	createdir(outfile)
 	outsize = imagesize(infile)	# 计算原始图片占用空间
 	if outsize > sizeKB:	# 若输出图片占用空间大于目标空间
 		im = Image.open(infile)
@@ -31,22 +43,34 @@ def compress(infile, outfile, sizeKB, xout, step=10, quality=80):	# 图片压缩
 	else:	# 只要小于目标尺寸，则直接复制一份到输出文件夹
 		shutil.copy(infile, outfile)
 
-def readimage(dirin, dirout, sizeKB, xout):	# 读取图片，输出图片
+def readimage(imagein, imageout, sizeKB, xout):	# 读取图片，输出图片
+	print(imagein)
+	compress(imagein, imageout, sizeKB, xout)	# 进行图片压缩
+
+def readimagelist(dirin, dirout):	# 读取所有文件，存入list中
 	if os.path.exists(dirin):	# 若输入图片的文件夹存在，否则建立文件夹
-		if not os.path.exists(dirout):	# 若输出图片的文件夹不存在则建立文件夹
-			os.mkdir(dirout)
-		for file in os.listdir(dirin):	# 读取输入图片的文件夹中的每一张图片
-			print(file)
-			infile = dirin + '/' + file
-			outfile = dirout + '/' + file
-			compress(infile, outfile, sizeKB, xout)	# 进行图片压缩
+		files = os.listdir(dirin)
+		for file in files:
+			currentpath = os.path.join(dirin, file)	# 路径全名
+			outpath = currentpath.split("\\")
+			outpath[0] = dirout	# 将第一层目录替换
+			outpath = "\\".join(outpath)
+			if os.path.isdir(currentpath):
+				readimagelist(currentpath, dirout)
+			else:
+				listimagein.append(currentpath)	# 存入文件路径
+				listimageout.append(outpath)	# 生成输出文件路径
 	else:
 		os.mkdir(dirin)
 
-
 if __name__ == '__main__':
+	global listimagein,listimageout
+	listimagein = []
+	listimageout = []
 	dirin = 'in'	# 输入图片的文件夹
 	dirout = 'out'	# 输出图片的文件夹
 	sizeKB = 100	# 输出图片的最大值，默认100KB
 	xout = 1366	# 输出图片的x轴长度，默认1366px。 y轴通过动态计算得到
-	readimage(dirin, dirout, sizeKB, xout)
+	readimagelist(dirin, dirout)	# 读取所有图片文件
+	for i in range(0,len(listimagein)):	# 读取输入图片的文件夹中的每一张图片
+		readimage(listimagein[i], listimageout[i], sizeKB, xout)
